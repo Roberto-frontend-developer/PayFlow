8513f4ed03fb0ed8a7d9018e81c2dfc3a9375614
roberto <robertosilva.2@hotmail.com>
2021-10-31 17:09:55 -0300

    refactore project

---
+++ lib/modules/home/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appbar: AppBar(
+        title: Text("HOme Page")
+      ),
+    );
+  }
+}
