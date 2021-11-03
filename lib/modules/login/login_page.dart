d12bd25ff91fafe4b67eca679fa5338f99df8327
roberto <robertosilva.2@hotmail.com>
2021-11-03 19:27:54 -0300

    final project

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
-import 'package:payflow/shared/widgets/social_login.dart';
-
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppColors.background,
-      // ignore: sized_box_for_whitespace
       body: Container(
         width: size.width,
         height: size.height,
@@ -31,13 +31,13 @@ class _LoginPageState extends State<LoginPage> {
               color: AppColors.primary,
             ),
             Positioned(
-                top: 10,
+                top: 40,
                 left: 0,
                 right: 0,
                 child: Image.asset(
                   AppImages.person,
                   width: 208,
-                  height: 350,
+                  height: 300,
                 )),
             Positioned(
               bottom: size.height * 0.05,
@@ -57,24 +57,14 @@ class _LoginPageState extends State<LoginPage> {
                       style: TextStyles.titleHome,
                     ),
                   ),
-                  Padding(                    
+                  Padding(
                     padding:
-                      const  EdgeInsets.only(left:40, top:40, right:40),
-                      child: SocialLoginButton(
-                        onTap:() async{
-                          GoogleSignIn _googleSignIn = GoogleSignIn(
-                            scopes: [
-                             'email',
-                             ],
-                          ); 
-                        try {
-                          final response = await _googleSignIn.signIn();
-                            print(response);
-                        } catch (error) {
-                            print( error);
-                        } 
-                      }
-                    )              
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        controller.googleSignIn(context);
+                      },
+                    ),
                   )
                 ],
               ),
--- lib/shared/themes/app_images.dart
+++ lib/shared/themes/app_images.dart
@@ -1,5 +1,4 @@
-
-class AppImages{
+class AppImages {
   static const logoFull = "assets/images/logofull.png";
   static const logomini = "assets/images/logomini.png";
   static const union = "assets/images/union.png";
